8f9a73bfc048a68b1231f36bab2bf3ba821dfaa1
Chris Pettyjohn <chrismmpettyjohn>
2022-06-16 05:22:47 -0500

    Prettier

--- packages/vesna-api/src/session/bearer-token.service.ts
+++ packages/vesna-api/src/session/bearer-token.service.ts
@@ -1,7 +1,7 @@
 import JWT from 'jsonwebtoken';
 import {Injectable} from '@nestjs/common';
 import {hoursToMS} from '../common/hours-to-ms';
-import {JWT_EXPIRATION_IN_HOURS, JWT_SECRET} from '@vesna-task-manager/api';
+import {JWT_EXPIRATION_IN_HOURS, JWT_SECRET} from '../common/config.const';
 
 @Injectable()
 export class BearerTokenService {
