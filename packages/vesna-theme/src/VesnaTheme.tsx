c18961bf35809d810c98c912a8cd3256a5ded316
Chris Pettyjohn <chrismmpettyjohn>
2022-06-16 17:47:55 -0500

    begin conversion to mui

--- packages/vesna-theme/src/VesnaTheme.tsx
+++ packages/vesna-theme/src/VesnaTheme.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import './public/css/VesnaTheme.scss';
 import {VesnaThemeProps} from './VesnaTheme.types';
 import {SiteLayout} from './component/site-layout/SiteLayout';
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from '@mui/material/styles';
 
 export function VesnaTheme({children}: VesnaThemeProps) {
-  return <SiteLayout>{children}</SiteLayout>;
+  let theme = createTheme({
+    palette: {
+      primary: {
+        main: '#f5f8ff',
+      },
+    },
+  });
+  theme = responsiveFontSizes(theme);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <SiteLayout>{children}</SiteLayout>
+    </ThemeProvider>
+  );
 }
--- packages/vesna-theme/src/component/task-labels-tabs/TaskLabelsTabs.tsx
+++ packages/vesna-theme/src/component/task-labels-tabs/TaskLabelsTabs.tsx
@@ -1,17 +1,34 @@
-import React, {useContext} from 'react';
-import {Tab, Tabs} from 'react-bootstrap';
+import {Box, Tab, Tabs} from '@mui/material';
+import React, {useContext, useState} from 'react';
 import {taskContext} from '@vesna-task-manager/web';
 
 export function TaskLabelTabs() {
   const {taskLabels} = useContext(taskContext);
+  const [activeTab, setActiveTab] = useState(taskLabels?.[0]?.id);
 
   return (
-    <Tabs id="task-labels" className="mb-3">
-      {taskLabels?.map(_ => (
-        <Tab key={`tab_${_.id}`} eventKey={`tab_${_.id}`} title={_.name}>
-          {_.name}
-        </Tab>
-      ))}
-    </Tabs>
+    <Box sx={{borderBottom: 1, borderColor: 'divider'}}>
+      <Tabs
+        value={activeTab}
+        onChange={(e, value) => setActiveTab(value)}
+        aria-label="active task label"
+      >
+        {taskLabels?.map(_ => (
+          <Tab key={`tab_${_.id}`} label={_.name} value={_.id} />
+        ))}
+        <Tab
+          label={
+            <div className="d-flex">
+              <i
+                className="fa fa-plus-circle"
+                style={{marginRight: 4, marginTop: 2}}
+              />{' '}
+              Create
+            </div>
+          }
+          value="create_label"
+        />
+      </Tabs>
+    </Box>
   );
 }
