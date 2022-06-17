bf0791af0caf6b0e6c097363e19dabe1ccaa5f5f
Chris Pettyjohn <chrismmpettyjohn>
2022-06-17 03:12:56 -0500

    Remove unused sass import

--- demo/web/src/index.tsx
+++ demo/web/src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { VesnaWebApp } from './VesnaWebApp';
-import '@vesna-task-manager/theme/dist/public/css/vesna-theme.css';
 
 ReactDOM.render(<VesnaWebApp />, document.getElementById('root'));
